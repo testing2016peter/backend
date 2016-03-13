b8c5b388a58ac315d3e66b8981b1baec0df52dfe
peter <jy02927731@yahoo.com.hk>
2016-03-13 15:31:05 +0800

    make it better

---
+++ routes/users.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var AV = require('leanengine')
+
+
+//istaging home
+AV.initialize("Wqia15HnxcIAXH1Lk06m2n3Q-gzGzoHsz", "hwd81deBLAQDk1a3G4jTUKjY");
+
+/* GET users listing. */
+router.get('/login', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/signup', function(req, res, next) {
+
+
+});
+
+router.post('/signup', function(req, res, next) {
+
+
+    var username = req.body.username;
+    var password = req.body.password;
+
+	var user = new AV.User();
+	user.set('username', username);
+	user.set('password', password);
+
+	user.signUp().then(function(user) {
+	  console.log(user);
+  	  res.send('respond with a signup');
+	}, function(error) {
+	  // 失败了
+	  console.log('Error: ' + error.code + ' ' + error.message);
+	
+  	  res.send(error);
+	});
+
+});
+
+module.exports = router;
