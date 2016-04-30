f833dcfbe190eb1ad58dabf9fe92a91208088e9b
peter <jy02927731@yahoo.com.hk>
2016-04-30 20:05:26 +0800

    B. Reviewer: sig C. fEATURE c1-  dislike post is ready: DELETE /posts/:postId/like1 DELETE /posts/:postId/like2

--- routes/like.js
+++ routes/like.js
@@ -15,7 +15,7 @@ var requiredLogin = require('../module/requiredLogin.js')
 
 var likeFunction = function(postId,likeType) {
 	var likeType = likeType
-	var likeCount = likeType+"_count"
+	var likeCountStr = likeType+"_count"
 
 
     return new Promise(function(resolve, reject) {
@@ -34,11 +34,11 @@ var likeFunction = function(postId,likeType) {
             if (listUser.length >= 1) {
                 reject("user liked this post")
             } else {
-                var like_count = post.get(likeCount)
+                var like_count = post.get(likeCountStr)
                 var relation = post.relation(likeType);
 
                 relation.add(AV.User.current());
-                post.set(likeCount, like_count + 1)
+                post.set(likeCountStr, like_count + 1)
 
 
                 return post.save()
@@ -61,6 +61,72 @@ var like1Function = function(postId){
 var like2Function = function(postId){
 	return likeFunction(postId, "like2")
 }
+
+
+var disklikefunction = function(postId, likeType){
+
+	var likeType = likeType
+	var likeCountStr = likeType+"_count"
+
+    return new Promise(function(resolve, reject) {
+	    var query = new AV.Query(Post);
+
+	    query.get(postId).then(function(p) {
+
+	    	console.log("   ------ 1")
+
+	    	var like_count = p.get(likeCountStr)
+
+	    	p.relation(likeType).remove(AV.User.current());
+	    	p.set(likeCountStr,like_count)
+
+	    	return p.save()
+	    }).then(function(p2){
+	    	console.log("   ------ 2")
+	    	resolve(p2)
+        }, function(error) {
+        	reject(error)
+        })
+    })
+}
+
+
+var disklike1function = function(postId){
+	return disklikefunction(postId, "like1" )
+}
+var disklike2function = function(postId){
+	return disklikefunction(postId, "like2" )
+}
+
+router.delete('/posts/:postId/like1',requiredLogin, function(req, res, next) {
+
+    console.log('delete /post/:postId/like1: '+req.params.postId)
+
+    var postId = req.params.postId
+
+    disklike1function(postId).then(function(post){
+    	res.status(200).json(FILTER.post(post))
+	}, function(error) {
+		console.log(error)
+        res.status(400).send(error)
+	});
+
+});
+
+router.delete('/posts/:postId/like2',requiredLogin, function(req, res, next) {
+
+    console.log('delete /post/:postId/like2: '+req.params.postId)
+
+    var postId = req.params.postId
+
+    disklike1function(postId).then(function(post){
+    	res.status(200).json(FILTER.post(post))
+	}, function(error) {
+		console.log(error)
+        res.status(400).send(error)
+	});
+
+});
 
 router.post('/posts/:postId/like1',requiredLogin, function(req, res, next) {
 
